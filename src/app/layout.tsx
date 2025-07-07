d55229c06d09ffbef47a960cadc7a4d17506e630
ramankarthik <ramankarthik@gmail.com>
2025-07-07 16:07:01 -0700

    Remove Leaflet CSS import to fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 
-// Add Leaflet CSS
-import 'leaflet/dist/leaflet.css';
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
